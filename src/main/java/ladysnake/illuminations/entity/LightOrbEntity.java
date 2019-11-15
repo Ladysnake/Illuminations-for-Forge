501b5b74ff205f76c835340354286c4de448509d
Subaraki <robbeenaxel@hotmail.com>
2019-11-15 01:38:56 +0100

    decided to make lightorb an interface, maybe for futu1re uses port was mostly unnceesairy in the mods current state

--- src/main/java/ladysnake/illuminations/entity/LightOrbEntity.java
+++ src/main/java/ladysnake/illuminations/entity/LightOrbEntity.java
@@ -1,14 +1,6 @@
 package ladysnake.illuminations.entity;
 
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.MobEntity;
-import net.minecraft.world.World;
+public interface LightOrbEntity{
 
-public class LightOrbEntity extends MobEntity{
-
-    public LightOrbEntity(EntityType<? extends MobEntity> type, World world) {
-
-        super(type, world);
-    }
 
 }
