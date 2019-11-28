baeeec2d04d03122a41e0aa03f0491dc65a202f6
Subaraki <robbeenaxel@hotmail.com>
2019-11-28 09:46:47 +0100

    Implement random tick data watcher skip update method when game time % random tick is not 0

--- src/main/java/ladysnake/illuminations/entity/FireFlyEntity.java
+++ src/main/java/ladysnake/illuminations/entity/FireFlyEntity.java
@@ -30,6 +30,8 @@ public class FireFlyEntity extends LightOrbEntity implements IEntityAdditionalSp
     private static final DataParameter<Float> COLOR_MODIFIER = EntityDataManager.createKey(PlayerEntity.class, DataSerializers.FLOAT);
     private static final DataParameter<Float> ALPHA_MODIFIER = EntityDataManager.createKey(PlayerEntity.class, DataSerializers.FLOAT);
 
+    private static final DataParameter<Integer> RANDOM_TICK_OFFSET = EntityDataManager.createKey(PlayerEntity.class, DataSerializers.VARINT);
+
     protected boolean canDespawn;
     protected boolean isAttractedByLight;
     private Float nextAlphaGoal;
@@ -55,22 +57,25 @@ public class FireFlyEntity extends LightOrbEntity implements IEntityAdditionalSp
 
     // auto syncs values across server and client
     @Override
-    protected void registerData() {
+    protected void registerData()
+    {
 
         super.registerData();
         this.dataManager.register(SCALE_MODIFIER, 1F);
         this.dataManager.register(COLOR_MODIFIER, 1F);
         this.dataManager.register(ALPHA_MODIFIER, 1F);
-
+        this.dataManager.register(RANDOM_TICK_OFFSET, 20);
     }
 
     // set data once, and only when joining the world. this one is called and sets
     // the scale and color correctly. onInitialSpawn isnt being called AFAIK
     @Override
-    public void onAddedToWorld() {
+    public void onAddedToWorld()
+    {
 
         this.dataManager.set(SCALE_MODIFIER, 0.1F + this.rand.nextFloat() * 0.15F);
         this.dataManager.set(COLOR_MODIFIER, 0.25F + this.rand.nextFloat() * 0.75F);
+        this.dataManager.set(RANDOM_TICK_OFFSET, this.rand.nextInt(20) + 20);
 
         super.onAddedToWorld();
     }
@@ -78,72 +83,86 @@ public class FireFlyEntity extends LightOrbEntity implements IEntityAdditionalSp
     // this one is never called to my testing. need to find out where it comes from.
     // will keep the info here anyway
     @Override
-    public ILivingEntityData onInitialSpawn(IWorld worldIn, DifficultyInstance difficultyIn, SpawnReason reason, ILivingEntityData spawnDataIn, CompoundNBT dataTag) {
+    public ILivingEntityData onInitialSpawn(IWorld worldIn, DifficultyInstance difficultyIn, SpawnReason reason, ILivingEntityData spawnDataIn, CompoundNBT dataTag)
+    {
 
         this.dataManager.set(SCALE_MODIFIER, 0.1F + this.rand.nextFloat() * 0.15F);
         this.dataManager.set(COLOR_MODIFIER, 0.25F + this.rand.nextFloat() * 0.75F);
+        this.dataManager.set(RANDOM_TICK_OFFSET, this.rand.nextInt(20) + 20);
 
         return super.onInitialSpawn(worldIn, difficultyIn, reason, spawnDataIn, dataTag);
     }
 
-    public float getScaleModifier() {
+    public float getScaleModifier()
+    {
 
         return this.dataManager.get(SCALE_MODIFIER);
     }
 
-    public float getColorModifier() {
+    public float getColorModifier()
+    {
 
         return this.dataManager.get(COLOR_MODIFIER);
     }
 
-    public float getAlpha() {
+    public float getAlpha()
+    {
 
         return this.dataManager.get(ALPHA_MODIFIER);
     }
 
-    public void setAlpha(float alpha) {
+    public void setAlpha(float alpha)
+    {
 
         this.dataManager.set(ALPHA_MODIFIER, alpha);
     }
 
-    public Float getNextAlphaGoal() {
+    public Float getNextAlphaGoal()
+    {
 
         return nextAlphaGoal;
     }
 
-    public void setNextAlphaGoal(Float nextAlphaGoal) {
+    public void setNextAlphaGoal(Float nextAlphaGoal)
+    {
 
         this.nextAlphaGoal = nextAlphaGoal;
     }
 
-    public boolean isAttractedByLight() {
+    public boolean isAttractedByLight()
+    {
 
         return isAttractedByLight;
     }
 
     @Override
-    public void writeAdditional(CompoundNBT compound) {
+    public void writeAdditional(CompoundNBT compound)
+    {
 
         compound.putFloat("scale", getScaleModifier());
         compound.putFloat("color", getColorModifier());
         compound.putFloat("alpha", getAlpha());
         compound.putBoolean("atracted", isAttractedByLight);
-
+        compound.putInt("tickoffset", dataManager.get(RANDOM_TICK_OFFSET));
+        
         super.writeAdditional(compound);
     }
 
     @Override
-    public void readAdditional(CompoundNBT compound) {
+    public void readAdditional(CompoundNBT compound)
+    {
 
         this.dataManager.set(SCALE_MODIFIER, compound.getFloat("scale"));
         this.dataManager.set(COLOR_MODIFIER, compound.getFloat("color"));
+        this.dataManager.set(RANDOM_TICK_OFFSET , compound.getInt("tickoffset"));
         setAlpha(compound.getFloat("alpha"));
 
         super.readAdditional(compound);
     }
 
     @Override
-    public boolean canSpawn(IWorld world, SpawnReason spawnReasonIn) {
+    public boolean canSpawn(IWorld world, SpawnReason spawnReasonIn)
+    {
 
         if (this.world.isDaytime())
             return false;
@@ -154,19 +173,21 @@ public class FireFlyEntity extends LightOrbEntity implements IEntityAdditionalSp
     }
 
     @Override
-    public void tick() {
+    public void tick()
+    {
 
         super.tick();
 
-        //only check once every second to prevent (some) lag
-        if (this.world.getGameTime() % 20 == 0) {
-            // kill when further away then x chunks
-            boolean anyPlayerCloseBy = world.isPlayerWithin(this.posX, this.posY, this.posZ, ConfigData.despawn_firefly);
+        // only check once every second to prevent (some) lag
+        if (this.world.getGameTime() % dataManager.get(RANDOM_TICK_OFFSET) >= 0)
+            return;
 
-            if (!anyPlayerCloseBy)
-                this.remove();
-        }
+        // kill when further away then x chunks
+        boolean anyPlayerCloseBy = world.isPlayerWithin(this.posX, this.posY, this.posZ, ConfigData.despawn_firefly);
 
+        if (!anyPlayerCloseBy)
+            this.remove();
+        
         long dayTime = this.world.getWorldInfo().getDayTime();
         if (dayTime > 1000 && dayTime < 12990)
             this.remove();
@@ -193,13 +214,17 @@ public class FireFlyEntity extends LightOrbEntity implements IEntityAdditionalSp
             this.move(MoverType.SELF, this.getMotion());
     }
 
-    private void selectBlockTarget() {
+    private void selectBlockTarget()
+    {
 
-        if (this.lightTarget == null || !this.isAttractedByLight()) {
+        if (this.lightTarget == null || !this.isAttractedByLight())
+        {
             this.groundLevel = 0;
-            for (int i = 0; i < 20; i++) {
+            for (int i = 0; i < 20; i++)
+            {
                 BlockState checkedBlock = this.world.getBlockState(new BlockPos(this.posX, this.posY - i, this.posZ));
-                if (!checkedBlock.getBlock().canSpawnInBlock()) {
+                if (!checkedBlock.getBlock().canSpawnInBlock())
+                {
                     this.groundLevel = this.posY - i;
                 }
                 if (this.groundLevel != 0)
@@ -215,12 +240,15 @@ public class FireFlyEntity extends LightOrbEntity implements IEntityAdditionalSp
 
             if (this.world.getLightFor(LightType.SKY, this.getPosition()) > 8 && !this.world.isDaytime())
                 this.lightTarget = getRandomLitBlockAround();
-        } else {
+        }
+        else
+        {
             this.xTarget = this.lightTarget.getX() + this.rand.nextGaussian();
             this.yTarget = this.lightTarget.getY() + this.rand.nextGaussian();
             this.zTarget = this.lightTarget.getZ() + this.rand.nextGaussian();
 
-            if (this.world.getLightFor(LightType.BLOCK, this.getPosition()) > 8) {
+            if (this.world.getLightFor(LightType.BLOCK, this.getPosition()) > 8)
+            {
                 BlockPos possibleTarget = getRandomLitBlockAround();
                 if (this.world.getLightFor(LightType.BLOCK, possibleTarget) > this.world.getLightFor(LightType.BLOCK, this.lightTarget))
                     this.lightTarget = possibleTarget;
@@ -233,10 +261,12 @@ public class FireFlyEntity extends LightOrbEntity implements IEntityAdditionalSp
         targetChangeCooldown = this.rand.nextInt() % 100;
     }
 
-    private BlockPos getRandomLitBlockAround() {
+    private BlockPos getRandomLitBlockAround()
+    {
 
         HashMap<BlockPos, Integer> randBlocks = new HashMap<>();
-        for (int i = 0; i < 15; i++) {
+        for (int i = 0; i < 15; i++)
+        {
             BlockPos randBP = new BlockPos(this.posX + this.rand.nextGaussian() * 10, this.posY + this.rand.nextGaussian() * 10,
                     this.posZ + this.rand.nextGaussian() * 10);
             randBlocks.put(randBP, this.world.getLightFor(LightType.BLOCK, randBP));
@@ -244,13 +274,15 @@ public class FireFlyEntity extends LightOrbEntity implements IEntityAdditionalSp
         return randBlocks.entrySet().stream().max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1).get().getKey();
     }
 
-    private BlockPos getTargetPosition() {
+    private BlockPos getTargetPosition()
+    {
 
         return new BlockPos(this.xTarget, this.yTarget + 0.5, this.zTarget);
     }
 
     @Override
-    public void writeSpawnData(PacketBuffer buffer) {
+    public void writeSpawnData(PacketBuffer buffer)
+    {
 
         buffer.writeFloat(getScaleModifier()); // scale
         buffer.writeFloat(getColorModifier());// color
@@ -258,7 +290,8 @@ public class FireFlyEntity extends LightOrbEntity implements IEntityAdditionalSp
     }
 
     @Override
-    public void readSpawnData(PacketBuffer buffer) {
+    public void readSpawnData(PacketBuffer buffer)
+    {
 
         this.dataManager.set(SCALE_MODIFIER, buffer.readFloat());
         this.dataManager.set(COLOR_MODIFIER, buffer.readFloat());
